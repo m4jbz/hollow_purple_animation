a3f58a87c8c71bd4a9f958350a2fcc007690d191
m4jbz <marcoajb05@proton.me>
2024-09-27 22:38:35 -0600

    kinda getting what I want, and a output.gif

--- README.md
+++ README.md
@@ -3,5 +3,7 @@
 ## First step
 
 [X] Draw two shapes moving and bouncing with corners of a square with raylib.
+
 [X] Make the circles have movement.
+
 [...] Make the collide animation itself.  
--- main.c
+++ main.c
@@ -3,45 +3,49 @@
 #include <math.h>
 #include "raylib.h"
 
-#define WIDTH 800
-#define HEIGHT 600
-#define H_WIDTH WIDTH/2.0f
-#define H_HEIGHT HEIGHT/2.0f
-#define PART_COUNT 16384/2
-#define RADIUS 70.0f
+#define WIDTH 800 // panel width
+#define HEIGHT 600 // panel height
+#define H_WIDTH WIDTH/2.0f // half width
+#define H_HEIGHT HEIGHT/2.0f // half height
+#define PART_COUNT 20*1024 // amount of particles
+#define RADIUS 50.0f 
 #define HOLLOW_PURPLE (Color) { 192, 0, 192, 255 }
 
 typedef struct {
-	Vector2 pos;
+	Vector2 pos; // position
 	Vector2 vel;
 	Color color;
 } Particle;
 
+// generates all particles initial position
 Particle gen_particles(Particle particle, Vector2 center, Color color);
-void draw(Particle** p, Particle* c);
-void free_all(Particle** p, Particle* c);
-int mem_is_safe(Particle* p);
-Vector2 do_friction(Vector2 vel, float amount);
-float get_dist(Vector2 pos, Vector2 otherPos);
+Vector2 attract(Vector2 pos, Vector2 vel, Vector2 center_pos, float value);
 Vector2 get_normal(Vector2 pos, Vector2 otherPos);
-Vector2 attract(Vector2 pos, Vector2 vel, Vector2 center_pos);
-Vector2 move(Vector2 pos, Vector2 vel);
+Vector2 do_friction(Vector2 vel, float amount);
+Vector2 move(Vector2 pos, Vector2 vel, int mode);
+float get_dist(Vector2 pos, Vector2 otherPos);
+// free all mememory
+void free_all(Particle** p, Particle* c);
+// main draw function
+void draw(Particle** p, Particle* c);
+// verifies if malloc fails
+int mem_is_safe(Particle* p);
 
 int main()
 {
 	Particle** particles = malloc(2 * sizeof(Particle*));
 	Particle* centers = malloc(2 * sizeof(Particle));
 	
-	if (!mem_is_safe(centers)) return 1;
-
 	for (int i = 0; i < 2; ++i)
 	{
 		particles[i] = malloc(PART_COUNT * sizeof(Particle));
 		if (!mem_is_safe(particles[i])) return 1;
 	}
 
-	centers[0].pos = (Vector2) { 200.0f, H_HEIGHT };
-	centers[1].pos = (Vector2) { WIDTH - 200.0f, H_HEIGHT };
+	if (!mem_is_safe(centers)) return 1;
+
+	centers[0].pos = (Vector2) { 100.0f, H_HEIGHT };
+	centers[1].pos = (Vector2) { WIDTH - 100.0f, H_HEIGHT };
 	centers[0].color = BLUE;
 	centers[1].color = RED;
 	
@@ -99,9 +103,9 @@ Vector2 get_normal(Vector2 pos, Vector2 otherPos) {
     return normal;
 }
 
-Vector2 attract(Vector2 pos, Vector2 vel, Vector2 center_pos)
+Vector2 attract(Vector2 pos, Vector2 vel, Vector2 center_pos, float value)
 {
-    float dist = fmax(get_dist(pos, center_pos), 0.3f);
+    float dist = fmax(get_dist(pos, center_pos), value);
     Vector2 normal = get_normal(pos, center_pos);
 
     vel.x -= normal.x/dist;
@@ -110,9 +114,14 @@ Vector2 attract(Vector2 pos, Vector2 vel, Vector2 center_pos)
 	return vel;
 }
 
-Vector2 move(Vector2 pos, Vector2 vel) {
-    pos.x += vel.x;
-    pos.y += vel.y;
+Vector2 move(Vector2 pos, Vector2 vel, int mode) {
+	if (mode == 1)
+		pos.x += vel.x-0.05f;
+
+	if (mode == 0)
+		pos.x += vel.x+0.05f;
+
+	pos.y += vel.y;
 
     if (pos.x < 0) pos.x += WIDTH;
     if (pos.x >= WIDTH) pos.x -= WIDTH;
@@ -126,28 +135,27 @@ void draw(Particle** p, Particle* c)
 {
 	InitWindow(WIDTH, HEIGHT, "Window");
 
-	SetTargetFPS(60);
+	SetTargetFPS(100);
 
 	int flag = 1;
+//	int frameNumber = 1;
 
 	while (!WindowShouldClose())
 	{
 		for (int i = 0; i < PART_COUNT; ++i)
 		{
-			p[0][i].vel = attract(p[0][i].pos, p[0][i].vel, c[0].pos);
-			p[1][i].vel = attract(p[1][i].pos, p[1][i].vel, c[1].pos);
+			p[0][i].vel = attract(p[0][i].pos, p[0][i].vel, c[0].pos, 0.2f);
+			p[1][i].vel = attract(p[1][i].pos, p[1][i].vel, c[1].pos, 0.2f);
 
-			p[0][i].vel = do_friction(p[0][i].vel, 0.95f);
-			p[1][i].vel = do_friction(p[1][i].vel, 0.95f);
+			p[0][i].vel = do_friction(p[0][i].vel, 0.9f);
+			p[1][i].vel = do_friction(p[1][i].vel, 0.9f);
 
-			p[0][i].pos = move(p[0][i].pos, p[0][i].vel);
-			p[1][i].pos = move(p[1][i].pos, p[1][i].vel);
+			p[0][i].pos = move(p[0][i].pos, p[0][i].vel, 1);
+			p[1][i].pos = move(p[1][i].pos, p[1][i].vel, 0);
 		}
 
 		BeginDrawing();
 		ClearBackground(BLACK);
-
-		DrawFPS(10.0f, 10.0f);
 
 		for (int i = 0; i < PART_COUNT; ++i)
 		{
@@ -170,15 +178,50 @@ void draw(Particle** p, Particle* c)
 			c[1].pos.x -= 2;
 		}
 
-		if (c[0].pos.x == c[1].pos.x)
+		if (c[0].pos.x+10.0f == c[1].pos.x-10.0f)
 		{
+			c[0].pos.x += 10.0f;
+			c[1].pos.x -= 10.0f;
+
 			for (int i = 0; i < PART_COUNT; ++i)
 			{
 				DrawPixelV(p[0][i].pos, HOLLOW_PURPLE);
 				DrawPixelV(p[1][i].pos, HOLLOW_PURPLE);
 			}
 			flag = 0;
+
+			for (int i = 0; i < PART_COUNT; ++i)
+			{
+				p[0][i].vel = attract(p[0][i].pos, p[0][i].vel, c[0].pos, 0.15f);
+				p[1][i].vel = attract(p[1][i].pos, p[1][i].vel, c[1].pos, 0.15f);
+
+				p[0][i].vel = do_friction(p[0][i].vel, 0.99f);
+				p[1][i].vel = do_friction(p[1][i].vel, 0.99f);
+
+				p[0][i].pos = move(p[0][i].pos, p[0][i].vel, 0);
+				p[1][i].pos = move(p[1][i].pos, p[1][i].vel, 1);
+			}
+
+			c[0].pos.x -= 10.0f;
+			c[1].pos.x += 10.0f;
 		}
+
+		if (H_WIDTH - c[0].pos.x < RADIUS && flag)
+		{
+			for (int i = 0; i < PART_COUNT; ++i)
+			{
+				if (p[0][i].pos.x > c[0].pos.x)
+					p[0][i].pos = move(p[0][i].pos, p[0][i].vel, 1);
+
+				if (p[1][i].pos.x < c[1].pos.x)
+					p[1][i].pos = move(p[1][i].pos, p[1][i].vel, 0);
+			}
+		}
+
+//		char filename[64];
+//		snprintf(filename, 64, "screenshot_%04d.png", frameNumber);
+//		TakeScreenshot(filename);  // Saves the current frame as an image
+//		frameNumber++;
 
 		EndDrawing();
 	}
